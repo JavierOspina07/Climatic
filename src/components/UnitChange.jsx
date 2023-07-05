54fcadae06e6c02d344bb0768bcec7607c7ff691
Javier Ospina <jaheosig10@gmail.com>
2023-07-04 20:07:55 -0500

    cambios de imagen

--- src/components/UnitChange.jsx
+++
@@ -1,1 +0,0 @@
-const UnitChange.jsx
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,52 +1,72 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import Icon from "ApideClima/public/iconos/1.svg";
 
 const WeatherCard = () => {
-  const [info, setInfo] = useState({});
+  const [data, setData] = useState({});
+  const [unit, setUnit] = useState("metric");
+  const icons = {
+    
+  }
 
   useEffect(() => {
-    axios
-      .get("https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
-      .then((resp) => {
-        console.log(resp.data);
-        setInfo(resp.data);
-      })
-      .catch((error) => console.error(error));
-  }, []);
+    navigator.geolocation.getCurrentPosition((position) => {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=f435644d8da0a624ff654c495d94ea50&lang=sp&units=metric`
+        )
+        .then((resp) => setData(resp.data))
+        .catch((error) => console.error(error));
+    });
+  }, [unit]);
+
+  const toggleUnit = () => {
+    setUnit(unit === "metric" ? "imperial" : "metric");
+  };
 
   return (
-    <div className="main_container">
+    <section className="main_container">
       <div className="title">
-        <h1>Weather App</h1>
+        <h1>Weather app</h1>
       </div>
-
-      <div className="search_bar">
-        <input type="text" placeholder="Buscar..." />
+      {/* barra de busqueda */}
+      <div class="group">
+        <svg class="bar" aria-hidden="true" viewBox="0 0 24 24">
+          <g>
+            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+          </g>
+        </svg>
+        <input placeholder="Buscar" type="search" class="input" />
       </div>
 
       <div className="weather_card">
-        <div className="icon_container">
-          <img src={Icon} alt="Icono" />
-        </div>
-        <h1 className="temperature">19°</h1>
+        <h1 className="temperature">
+          {unit === "metric"
+            ? data.main?.temp?.toFixed(2)
+            : ((data.main?.temp * 9) / 5 + 32).toFixed(2)}
+          {unit === "metric" ? " °C" : " °F"}
+          <img src={icons[data.weather?.[0].icon]} alt="iconos del tiempo" />
+        </h1>
         <div className="environmental_container">
-          <p>VIENTO</p>
-          <p>NUBES</p>
-          <p>PRESIÓN</p>
+          <p>VIENTO: {data.wind?.speed} m/s</p>
+          <p>NUBES: {data.clouds?.all} %</p>
+          <p>PRESIÓN: {data.main?.pressure} hPa </p>
         </div>
         <div className="bottom_row">
           <div className="city_container">
-            <p>Palmira, Colombia</p>
+            <p>
+              {data.name}, {data.sys?.country}
+            </p>
           </div>
           <div className="sky_status">
-            <p>Parcialmente nublado</p>
+            <p>{data.weather?.[0].description}</p>
           </div>
         </div>
       </div>
 
-      <button className="units_button">Unidades</button>
-    </div>
+      <button className="units_button" onClick={toggleUnit}>
+        <i className="fa-solid fa-temperature-low"></i>
+      </button>
+    </section>
   );
 };
 
