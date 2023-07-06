023fe95245ab5ebc333fa6f45e6bf5ba3b8d6c63
Javier Ospina <jaheosig10@gmail.com>
2023-07-06 08:57:18 -0500

    cambios de la extraccion del icono

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -4,26 +4,6 @@ import { useState, useEffect } from "react";
 const WeatherCard = () => {
   const [data, setData] = useState({});
   const [unit, setUnit] = useState("metric");
-  const icons = {
-    "01d": "/icons/01d.svg",
-    "02d": "/icons/02d.svg",
-    "03d": "/icons/03d.svg",
-    "04d": "/icons/04d.svg",
-    "09d": "/icons/09d.svg",
-    "10d": "/icons/10d.svg",
-    "11d": "/icons/11d.svg",
-    "13d": "/icons/13d.svg",
-    "50d": "/icons/50d.svg",
-    "01n": "/icons/01n.png",
-    "02n": "/icons/02n.png",
-    "03n": "/icons/03n.svg",
-    "04n": "/icons/04n.svg",
-    "09n": "/icons/09n.svg",
-    "10n": "/icons/10n.svg",
-    "11n": "/icons/11n.svg",
-    "13n": "/icons/13n.svg",
-    "50n": "/icons/50n.svg"
-  };
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -40,12 +20,15 @@ const WeatherCard = () => {
     setUnit(unit === "metric" ? "imperial" : "metric");
   };
 
+  const alt = data.weather && data.weather[0] && data.weather[0].description;
+  const icon = data?.weather?.[0].icon;
+
   return (
     <section className="main_container">
       <div className="title">
         <h1>Weather app</h1>
       </div>
-      
+
       <div className="group">
         <svg className="bar" aria-hidden="true" viewBox="0 0 24 24">
           <g>
@@ -63,7 +46,7 @@ const WeatherCard = () => {
           {unit === "metric" ? " °C" : " °F"}
         </h1>
         <div className="Climate_Image">
-          <img src={icons[data.weather?.[0].icon]} alt="iconos" />
+          <img src={`./icons/${icon}.svg`} alt={alt} className="icon_weather" />
         </div>
         <div className="environmental_container">
           <p>VIENTO: {data.wind?.speed} m/s</p>
