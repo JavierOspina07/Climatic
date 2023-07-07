019672c5709266440ed778887c180101e35a5f89
Javier Ospina <jaheosig10@gmail.com>
2023-07-07 13:45:46 -0500

    añadimos un loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import WeatherCard from "./components/WeatherCard"
 import DarkMode from './components/DarkMode'
-
+import Loader from './components/Loader'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [isloading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    // Simulación de carga asincrónica
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  }, []);
+
   return (
     <>
+      {isloading &&    <Loader />}
       <DarkMode/>
       <WeatherCard/>
     </>
